8cd55c758170595a544b940329f5d07a6d27c3d5
ImangaliK <imangalikuanysh99@gmail.com>
2022-11-07 23:14:47 +0600

    added class

--- schemas.py
+++ schemas.py
@@ -1,1 +1,11 @@
 from pydantic import BaseModel
+
+class Customer(BaseModel):
+    user_id: int
+    role: str
+    address: str
+    username: str
+    password: str
+
+    class Config:
+        orm_mode = True
