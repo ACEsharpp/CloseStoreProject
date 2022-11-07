04c6b16d2eead2ff3bb80af6f39614bed1874545
Batyrkhan17 <117516974+Batyrkhan17@users.noreply.github.com>
2022-11-07 23:10:01 +0600

    Create post_email.py

---
+++ post_email.py
@@ -0,0 +1,26 @@
+import smtplib
+from os.path import basename
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from email.mime.application import MIMEApplication
+
+def send(to_address, subject, text):
+    from_add = 'kamow2014@mail.ru'
+    to_add = to_address
+    subject = subject
+    text = text
+
+    msg = MIMEMultipart()
+    msg['From'] = from_add
+    msg['To'] = to_add
+    msg['Subject'] = subject
+    body = MIMEText(text, 'plain')
+    msg.attach(body)
+    server = smtplib.SMTP('smtp.mail.ru', port=587)
+    server.connect("smtp.mail.ru",587)
+    server.ehlo()
+    server.starttls()
+    server.ehlo()
+    server.login(from_add,'3bmXkubL93yjGvDifYDP')
+    server.send_message(msg, from_addr=from_add, to_addrs=[to_add])
+    server.quit()
