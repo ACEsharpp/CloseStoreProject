7d22d9df86039f3720c68e8f2a4b2bbf3186a5a8
ACEsharpp <zharaspaev03@mail.ru>
2022-11-07 23:34:18 +0600

    uploading schemas py(expanded version)

--- schemas.py
+++ schemas.py
@@ -1,1 +1,47 @@
 from pydantic import BaseModel
+
+
+from pydantic import BaseModel
+
+
+class Customer(BaseModel):
+    user_id: int
+    role: str
+    address: str
+    username: str
+    password: str
+
+    class Config:
+        orm_mode = True
+
+
+class Favorites(BaseModel):
+    favorite_id: int
+    customer_id: int
+    product_id: int
+
+    class Config:
+        orm_mode = True
+
+class Card(BaseModel):
+    card_id: int
+    customer_id: int
+    product_id: int
+
+    class Config:
+        orm_mode = True
+
+
+class Product(BaseModel):
+    product_id: int
+    owner_id: int
+    name: str
+    price: int
+    color: str
+    photo: str
+    type: str
+    model: str
+    country: str
+
+    class Config:
+        orm_mode = True
