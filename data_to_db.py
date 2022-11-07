2a348c03931594c12f0a9782db56750264891741
Batyrkhan17 <117516974+Batyrkhan17@users.noreply.github.com>
2022-11-07 23:31:22 +0600

    Update data_to_db.py

--- data_to_db.py
+++ data_to_db.py
@@ -26,3 +26,55 @@ with open("data/clothes.json", encoding='UTF-8') as f:
 with open("data/shoes.json", encoding='UTF-8') as f:
     shoes = json.load(f)
 products = []
+for type in clothes.keys():
+    for id in clothes[type].keys():
+        model = None
+        if type == 'Футболки':
+            model = 'T-SHIRT'
+        if type == 'Кофты':
+            model = 'SWEATSHIRT'
+        if type == 'Джинсы':
+            model = 'JEANS'
+        if type == 'Шорты':
+            model = 'SHORTS'
+        if model is None:
+            print(f"Model is None {type}")
+        products.append(product(
+            name=clothes[type][id]["name"],
+            country=None,
+            photo=clothes[type][id]['photo'],
+            type=clothes[type][id]['type'],
+            color=clothes[type][id]['color'],
+            model=model,
+            price=clothes[type][id]['price']
+        ))
+
+
+for type in shoes.keys():
+    for id in shoes[type].keys():
+        model = None
+        if type == 'Кроссовки':
+            model = 'SNEAKERS'
+        if type == 'Туфли':
+            model = 'SHOES'
+        if model is None:
+            print(f"Model is None {type}")
+        products.append(product(
+            name=shoes[type][id]["name"],
+            country=shoes[type][id]['Country of Origin'],
+            photo=shoes[type][id]['Photo'],
+            type=model,
+            color=shoes[type][id]['color'],
+            model=model,
+            price=shoes[type][id]['price']
+        ))
+sql = f"INSERT INTO public.users(role, username, password) VALUES ('seller', 'admin@gmail.com', 'admin');"
+cursor.execute(sql)
+connection.commit()
+for product in products:
+    product_name = product.name.replace("'","`")
+    sql = f"INSERT INTO public.products(name,price, color, photo, type, model, country, owner_id)" \
+          f"VALUES ('{product_name}', {product.price}, '{product.color}', '{product.photo}', '{product.type}', '{product.model}', '{product.country}',1);"
+    cursor.execute(sql)
+
+connection.commit()
